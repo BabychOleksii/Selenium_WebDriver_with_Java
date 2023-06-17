5903f556d996067ece734d6ff3655ca00f953471
@BabychOleksii <babych.oleksii@gmail.com>
2023-06-17 16:52:07 +0300

    Actions class

--- Introduction/src/O85_MouseInteractions.java
+++ Introduction/src/O85_MouseInteractions.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -9,13 +10,16 @@ public class O85_MouseInteractions {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com/");
 		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");		
 		
 		Actions my_act = new Actions(driver);
 		my_act.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
 		
-
+		my_act.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("lenovo").doubleClick().build().perform();
+		
+		my_act.moveToElement(driver.findElement(By.id("nav-orders"))).contextClick().build().perform();
+		
 	}
 
 }
