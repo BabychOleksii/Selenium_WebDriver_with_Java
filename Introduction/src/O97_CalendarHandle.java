270204952f76fd28cd75227b852d7518a69326cf
@BabychOleksii <babych.oleksii@gmail.com>
2023-06-18 12:06:31 +0300

    Handle the day of further month

--- Introduction/src/O97_CalendarHandle.java
+++ Introduction/src/O97_CalendarHandle.java
@@ -19,26 +19,35 @@ public class O97_CalendarHandle {
 		// Scroll the Page
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("window.scrollBy(0,1100)");
-		Thread.sleep(3000);
-		
-		// get the 23 day of the current month, which is displayed on the opened calendar tab
-//		driver.findElement(By.id("form-field-travel_comp_date")).click();
+		Thread.sleep(3000);		
 		driver.findElement(By.xpath(".//*[@id='form-field-travel_comp_date']")).click();
 		
-		List<WebElement> dates = driver.findElements(By.cssSelector(".flatpickr-day"));
+//		// get the 23 day of the current month, which is displayed on the opened calendar tab
+//		int days_count = driver.findElements(By.cssSelector(".flatpickr-day")).size();
+//		for (int i=0; i < days_count; i++) {
+//			String day_text = driver.findElements(By.cssSelector(".flatpickr-day")).get(i).getAttribute("innerHTML");
+//			if (day_text.equalsIgnoreCase("23")) {
+//				driver.findElements(By.cssSelector(".flatpickr-day")).get(i).click();
+//				break;
+//			}
+//		}
 		
+		// get the 23 January day of the next year
+//		System.out.println(driver.findElement(By.cssSelector(".cur-month")).getAttribute("innerHTML"));
+		while (!driver.findElement(By.cssSelector(".cur-month")).getAttribute("innerHTML").contains("January")) {
+			driver.findElement(By.cssSelector(".flatpickr-next-month")).click();
+			Thread.sleep(500);
+		}
+//		System.out.println(driver.findElement(By.cssSelector(".cur-month")).getAttribute("innerHTML"));
 		int days_count = driver.findElements(By.cssSelector(".flatpickr-day")).size();
-		System.out.println(days_count);
 		for (int i=0; i < days_count; i++) {
-			System.out.println(i);
 			String day_text = driver.findElements(By.cssSelector(".flatpickr-day")).get(i).getAttribute("innerHTML");
-			System.out.println("day_text = " + day_text);
 			if (day_text.equalsIgnoreCase("23")) {
 				driver.findElements(By.cssSelector(".flatpickr-day")).get(i).click();
 				break;
 			}
 		}
-
+	
+//		List<WebElement> dates = driver.findElements(By.cssSelector(".flatpickr-day"));
 	}
-
 }
