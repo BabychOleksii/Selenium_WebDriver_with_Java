a9ed7c08a83d512c7628c7a561b75750dc1c9ab3
@BabychOleksii <babych.oleksii@gmail.com>
2023-06-19 11:18:24 +0300

    Delete the cookie

--- Introduction/src/O104_HandingHTTPSCertificates.java
+++ Introduction/src/O104_HandingHTTPSCertificates.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,12 +9,24 @@ public class O104_HandingHTTPSCertificates {
 		// TODO Auto-generated method stub
 		
 		ChromeOptions my_options = new ChromeOptions();
+		
+		// overcome the HPPS certificate
 		my_options.setAcceptInsecureCerts(true);
 		
+		// add Proxy
+//		Proxy my_proxy = new Proxy();
+//		my_proxy.setHttpProxy("ipaddress:4444");
+//		my_options.setCapability("proxy", my_proxy);
+		
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://expired.badssl.com/");
-		driver.manage().window().maximize();
+		
+		driver.manage().window().maximize(); // maximize the browser window		
+		driver.manage().deleteCookieNamed("sessionKey"); // delete the specific cookie
+		driver.manage().deleteAllCookies(); // delete all cookies
+		
+		driver.get("https://google.com/");		
 		System.out.println(driver.getTitle());
+		
 
 	}
 
