c06d7d1a8bd5c9e9c12d9e9f73acd257bd6ff647
@BabychOleksii <babych.oleksii@gmail.com>
2023-06-30 11:57:52 +0300

    153 Find the item and add it to the cart

--- SeleniumFrameworkDesign/src/test/java/JavaAutomationTesting/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/JavaAutomationTesting/StandAloneTest.java
@@ -14,18 +14,26 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://rahulshettyacademy.com/client/");
-		
+
 		// login into account
 		driver.findElement(By.id("userEmail")).sendKeys("babych.oleksii@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("babych.oleksii@gmail.comM1");
 		driver.findElement(By.id("login")).click();
+
+		// find the item and add to cart
+		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
+		WebElement prod = products.stream()
+				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals("ZARA COAT 3")).findFirst()
+				.orElse(null);
+		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
 		
+
 	}
 
 }
