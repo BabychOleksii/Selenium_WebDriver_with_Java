3db6b24191ea7d68bacd7237967880b5c4e77059
@BabychOleksii <babych.oleksii@gmail.com>
2023-06-30 16:00:22 +0300

    156 Fill the dropdaown in checkout and verify the 'thank you' message

--- SeleniumFrameworkDesign/src/test/java/JavaAutomationTesting/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/JavaAutomationTesting/StandAloneTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -17,44 +18,56 @@ public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		String productName = "ZARA COAT 3";
 
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/client/");
 
 		// login into account
 		driver.findElement(By.id("userEmail")).sendKeys("anyuser@gmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("anyuser@gmail.comM1");
 		driver.findElement(By.id("login")).click();
-		
+
 		// find the item and add to cart
-		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
 		WebElement prod = products.stream()
 				.filter(product -> product.findElement(By.cssSelector("b")).getText().equals(productName)).findFirst()
 				.orElse(null);
 		prod.findElement(By.cssSelector(".card-body button:last-of-type")).click();
-		
+
 		// check the message that the item was added to the cart
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
 		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
-		
+
 		// move to the cart page
 		driver.findElement(By.cssSelector("[routerlink*='cart']")).click();
-		
+
 		// check the products in the cart page
 		List<WebElement> cartProducts = driver.findElements(By.cssSelector(".cartSection h3"));
-		Boolean match = cartProducts.stream().anyMatch(cartProduct->cartProduct.getText().equalsIgnoreCase(productName));
+		Boolean match = cartProducts.stream()
+				.anyMatch(cartProduct -> cartProduct.getText().equalsIgnoreCase(productName));
 		Assert.assertTrue(match);
-		
+
 		// move to the checkout page
 		driver.findElement(By.cssSelector(".totalRow button")).click();
 
+		// fill the required fields on the checkout page (choose a country from dropdown)
+		Actions act = new Actions(driver);
+		act.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		driver.findElement(By.xpath("(//button[contains(@class, 'ta-item')])[2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		// Check the confirmation message on the "thank you" page
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
 	}
 
 }
